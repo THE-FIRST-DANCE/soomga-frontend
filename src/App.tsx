60922f45049aca85b549072160fb82d1d895a5f1
cheiru94 <cheiru94@gmail.com>
2024-02-01 01:49:34 +0900

    feat: CSS 초기화 및 구조정리

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,10 @@ import { createGlobalStyle } from 'styled-components' // CSS 초기화
 const GlobalStyle = createGlobalStyle`
   ${reset}
   /* other styles */
+  :root {
+    --color-primary: #FFD766;
+  }
+
 `
 
 function App() {
