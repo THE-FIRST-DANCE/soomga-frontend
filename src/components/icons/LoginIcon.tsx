6d9d2e1e642ec7c9d5143cfb16f57b3d9b65f1af
cheiru94 <cheiru94@gmail.com>
2024-02-01 01:53:09 +0900

    feat: NavBar 아이콘

---
+++ src/components/icons/LoginIcon.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+interface LoginProps {
+  width?: string
+  height?: string
+  color?: string
+  hoverColor?: string
+}
+
+const LoginIcon = ({ ...props }: LoginProps) => {
+  return (
+    <LoginStyle {...props} viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+      <title />
+      <path d="M256,48C141.31,48,48,141.31,48,256s93.31,208,208,208,208-93.31,208-208S370.69,48,256,48Zm2,96a72,72,0,1,1-72,72A72,72,0,0,1,258,144Zm-2,288a175.55,175.55,0,0,1-129.18-56.6C135.66,329.62,215.06,320,256,320s120.34,9.62,129.18,55.39A175.52,175.52,0,0,1,256,432Z" />
+    </LoginStyle>
+  )
+}
+
+const LoginStyle = styled.svg<LoginProps>`
+  width: ${(props) => props.width || '48px'};
+  height: ${(props) => props.height || '48px'};
+  fill: ${(props) => props.color || 'black'};
+  &:hover {
+    fill: ${(props) => props.hoverColor || 'black'};
+  }
+`
+export default LoginIcon
