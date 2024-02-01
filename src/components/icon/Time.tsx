c519802e130a6aa89022e60a17fb32f882c5c014
hetame1 <poi8946@naver.com>
2024-02-01 14:51:44 +0900

    feat: 공통 아이콘 추가
    화살표, 별, 시간 아이콘 svg 형식으로 추가

---
+++ src/components/icon/Time.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+interface TimeProps {
+  $width?: string
+  $height?: string
+  $color?: string
+}
+
+const Time = ({ ...props }: TimeProps) => {
+  return (
+    <TimeStyle {...props} viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+      <g data-name="Layer 15" id="Layer_15">
+        <path
+          className="cls-1"
+          d="M16,31A15,15,0,1,1,31,16,15,15,0,0,1,16,31ZM16,3A13,13,0,1,0,29,16,13,13,0,0,0,16,3Z"
+        />
+        <path className="cls-1" d="M20.24,21.66l-4.95-4.95A1,1,0,0,1,15,16V8h2v7.59l4.66,4.65Z" />
+      </g>
+    </TimeStyle>
+  )
+}
+
+export default Time
+
+const TimeStyle = styled.svg<TimeProps>`
+  width: ${(props) => props.$width};
+  height: ${(props) => props.$height};
+  fill: ${(props) => props.$color};
+`
