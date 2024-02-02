12447b90a94cd27d06712820fcb3bc60ea86f630
cheiru94 <cheiru94@gmail.com>
2024-02-02 18:04:22 +0900

    feat(hooks): ClickOutsideToggle 커스텀 훅 헤더 우측 드롭다운 클릭시, 외부를 클릭했을 때 드롭다운 off시켜 준다.

---
+++ src/hooks/useClickOutsideToggle.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useRef, useState } from 'react'
+
+const useClickOutsideToggle = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const refForToggle = useRef<HTMLDivElement>(null)
+
+  // 토글 클릭
+  const handleOnClick = () => {
+    setIsOpen(!isOpen)
+  }
+
+  // 토글 밖 클릭
+  const handleOutClick = (e: MouseEvent) => {
+    /* 토글상태  &&  Ref가 유효한 요소를 가리키는지 체크 && 클릭을 토글 밖에서 한지 */
+    if (isOpen && refForToggle.current && !refForToggle.current.contains(e.target)) {
+      setIsOpen(false)
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleOutClick)
+    return () => {
+      // Unmount,isOpen변경 時 실행
+      document.removeEventListener('mousedown', handleOutClick)
+    }
+  }, [isOpen])
+
+  return { isOpen, refForToggle, handleOnClick }
+}
+
+export default useClickOutsideToggle
