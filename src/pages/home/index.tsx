39ea494f831df5a9385317ff4c1f945a4c12682a
cheiru94 <cheiru94@gmail.com>
2024-02-08 00:52:39 +0900

    feat: 메인페이지 우리나라 지도 (임시 네이버 사용)

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,25 @@
 import MainVideo from 'components/home/MainVideo'
 import RecommendedRegions from 'components/home/RecommendedRegions'
+import Map from 'components/planner/Map'
+import { styled } from 'styled-components'
 
 const MainPage = () => {
   return (
     <>
       <MainVideo />
       <RecommendedRegions />
+      <InnerContainer_div>
+        <Map width="70%" height="600px" zoom={7} />
+      </InnerContainer_div>
     </>
   )
 }
 
 export default MainPage
+
+const InnerContainer_div = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 150px 0;
+`
