431df810e4baac5f876e707b266e50303231b80f
hetame1 <poi8946@naver.com>
2024-02-13 16:08:00 +0900

    feat: 플랜 확인에 보낼 리스트와 교통수단 리코일 작업

--- src/recoil/atoms/PlanList.ts
+++ src/recoil/atoms/PlanList.ts
@@ -8,7 +8,20 @@ export interface PlanListRecoil {
   checked: boolean
 }
 
+export interface PlanListConfirm {
+  planList: PlanListRecoil[]
+  transportation: string
+}
+
 export const PlanListRecoil = atom<PlanListRecoil[]>({
   key: 'PlanList',
   default: [],
+})
+
+export const PlanListConfirm = atom<PlanListConfirm>({
+  key: 'PlanListConfirm',
+  default: {
+    planList: [],
+    transportation: '',
+  },
 })
