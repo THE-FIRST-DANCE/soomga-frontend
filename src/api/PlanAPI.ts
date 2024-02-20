52da7cddd3957f00a12126c449d26e31294800d2
hetame1 <poi8946@naver.com>
2024-02-20 19:18:06 +0900

    refactor: 날짜별 일정 짜도록 수정
    날짜별 플랜 따로 짜게 값 수정
    플랜 만들때 시간 x 기간 받게 변경

--- src/api/PlanAPI.ts
+++ src/api/PlanAPI.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { GooglePlaceResponse, PlaceData } from 'interfaces/plan'
+import { PeriodPlanRecoil } from 'recoil/atoms/PlanList'
 
 const api = axios.create({
   baseURL: 'http://localhost:3000/',
@@ -30,6 +31,22 @@ export const getPlaceApi = async (category: string, region: string) => {
       region,
     },
   })
+
+  return response.data
+}
+
+interface PlanListResponse {
+  list: PeriodPlanRecoil
+  transport: string
+}
+
+export const getPlaceRoute = async ({ list, transport }: PlanListResponse) => {
+  const data = {
+    list,
+    transport,
+  }
+
+  const response = await api.post('plans/distance', data)
 
   return response.data
 }
--- src/hooks/useCreatePlan.tsx
+++ src/hooks/useCreatePlan.tsx
@@ -10,8 +10,7 @@ const useCreatePlan = () => {
   const [openProvince, setOpenProvince] = useState<boolean>(false)
 
   const [planTitle, setPlanTitle] = useState<string>('')
-  const [startTime, setStartTime] = useState<string>('')
-  const [endTime, setEndTime] = useState<string>('')
+  const [period, setPeriod] = useState<number>(1)
 
   const setPlanInfo = useSetRecoilState(PlanInfo)
 
@@ -24,17 +23,13 @@ const useCreatePlan = () => {
       setPlanTitle(value)
     }
 
-    if (name === 'startTime') {
-      setStartTime(value)
-    }
-
-    if (name === 'endTime') {
-      setEndTime(value)
+    if (name === 'period') {
+      setPeriod(Number(value))
     }
   }
 
   const createPlan = () => {
-    if (!planTitle || !province || !startTime || !endTime || !lat || !lng) {
+    if (!planTitle || !province || !period || !lat || !lng) {
       alert('모든 항목을 입력해주세요')
       return
     }
@@ -42,8 +37,7 @@ const useCreatePlan = () => {
     setPlanInfo({
       title: planTitle,
       province,
-      startTime,
-      endTime,
+      period,
       lat,
       lng,
     })
@@ -56,7 +50,7 @@ const useCreatePlan = () => {
     setProvince,
     openProvince,
     setOpenProvince,
-    setEndTime,
+    setPeriod,
     onChange,
     createPlan,
     setLat,
