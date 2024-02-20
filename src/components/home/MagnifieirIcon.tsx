f595370fce33170ee2c5991c0f75cf8a443ef7ba
cheiru94 <cheiru94@gmail.com>
2024-02-21 00:47:23 +0900

    style: 돋보기 아이콘

--- src/components/home/MagnifieirIcon.tsx
+++ src/components/home/MagnifieirIcon.tsx
@@ -4,7 +4,6 @@ interface MagnifieirProps {
   style: CSSProp
 }
 const MagnifieirIcon = ({ ...props }: MagnifieirProps) => {
-  console.log('ss', props)
   return (
     <MagnifieirStyle {...props} version="1.1" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/MagnifieirStyle">
       <g id="info" />
