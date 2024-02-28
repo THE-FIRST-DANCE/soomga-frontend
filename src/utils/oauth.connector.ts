e3cd8e983f42f72ed9eadcd63a16be91429df31c
cheiru94 <cheiru94@gmail.com>
2024-02-28 17:36:43 +0900

    feat: 구글, 라인 백 요청

---
+++ src/utils/oauth.connector.ts
@@ -0,0 +1,17 @@
+type OAuthProvider = 'google' | 'line'
+
+export function onOAuthConnect(provider: OAuthProvider, redirectUrl?: string) {
+  const url = `${import.meta.env.VITE_APP_BASE_URL}/auth/${provider}`
+
+  window.sessionStorage.setItem('redirect', redirectUrl || window.location.pathname)
+
+  window.open(url, '_self')
+}
+
+export function onGoogleLogin(redirectUrl?: string) {
+  onOAuthConnect('google', redirectUrl)
+}
+
+export function onLineLogin(redirectUrl?: string) {
+  onOAuthConnect('line', redirectUrl)
+}
