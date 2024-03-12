25569143d505a740871f3b2169df54c8cc2ba626
hetame1 <poi8946@naver.com>
2024-03-12 21:39:11 +0900

    feat: 코드 리팩터링, 생성 모달, 상세보기 모달 작업

--- src/pages/schedulePage/index.tsx
+++ src/pages/schedulePage/index.tsx
@@ -1,216 +1,48 @@
 import styled from 'styled-components'
-import { Calendar, View, NavigateAction, Navigate, momentLocalizer } from 'react-big-calendar'
-import { Calendar as CalendarComponent, OnArgs } from 'react-calendar'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import CustomToolbar from 'components/schedule/CustomToolbar'
 
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import moment from 'moment'
-import Arrow from 'components/icons/Arrow'
-import { useState } from 'react'
-
-const testEvnets = [
-  {
-    title: 'test1',
-    start: new Date(2024, 2, 5),
-    end: new Date(2024, 2, 6),
-  },
-  {
-    title: 'test2',
-    start: new Date(2024, 2, 6),
-    end: new Date(2024, 2, 7),
-  },
-  {
-    title: 'test3',
-    start: new Date(2024, 2, 7),
-    end: new Date(2024, 2, 9),
-  },
-]
-
-interface CustomToolbarProps {
-  label: string
-  onNavigate: (action: NavigateAction, newDate?: Date) => void
-  onView: (view: View) => void
-  view: View
-}
-
-const CustomToolbar = ({ label, onNavigate, onView, view }: CustomToolbarProps) => {
-  const [open, setOpen] = useState<boolean>(false)
-
-  return (
-    <ToolbarContainer>
-      <Tool>
-        <TodayButton onClick={() => onNavigate(Navigate.TODAY)}>Today</TodayButton>
-      </Tool>
-
-      <DayText>
-        {label}
-        <div style={{ cursor: 'pointer' }} onClick={() => setOpen(!open)}>
-          <Arrow $width="1.5rem" $height="1.5rem" $angle="90deg" />
-        </div>
-
-        {open && (
-          <DaySelect>
-            <SetDateCalendar setOpen={setOpen} date={new Date(label)} onNavigate={onNavigate} />
-          </DaySelect>
-        )}
-      </DayText>
-
-      <Tool
-        style={{
-          justifyContent: 'flex-end',
-          gap: '1rem',
-        }}
-      >
-        <FilterSelect className={view === 'month' ? 'active' : ''} onClick={() => onView('month')}>
-          Month
-        </FilterSelect>
-        <FilterSelect className={view === 'week' ? 'active' : ''} onClick={() => onView('week')}>
-          Week
-        </FilterSelect>
-      </Tool>
-    </ToolbarContainer>
-  )
-}
-
-const ToolbarContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 1.5rem;
-  width: 100%;
-`
-
-const TodayButton = styled.button`
-  background-color: transparent;
-  color: var(--bs-info);
-  border: none;
-  font-size: 1rem;
-  cursor: pointer;
-`
-
-const DaySelect = styled.div`
-  position: absolute;
-  top: 100%;
-  left: 50%;
-  transform: translateX(-50%);
-  border-radius: 0.5rem;
-  z-index: 100;
-`
-
-const DayText = styled.div`
-  position: relative;
-  flex: 1;
-  text-align: center;
-  font-size: 1.5rem;
-  font-weight: 600;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 0.5rem;
-`
-
-const Tool = styled.div`
-  display: flex;
-  flex: 1;
-`
-
-const FilterSelect = styled.button`
-  background-color: transparent;
-  color: var(--bs-gray-800);
-  border: 1px solid var(--bs-gray-300);
-  font-size: 1rem;
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  gap: 0.5rem;
-  padding: 0.8rem;
-  border-radius: 0.5rem;
-  transition: 0.3s;
-
-  &:hover {
-    background-color: var(--bs-gray-100);
-  }
-
-  &.active {
-    background-color: var(--color-primary);
-    color: var(--bs-gray-dark);
-    border: 1px solid var(--bs-black);
-  }
-`
-
-interface SetDateCalendarProps {
-  onNavigate: (action: NavigateAction, newDate: Date) => void
-  date: Date
-  setOpen: (open: boolean) => void
-}
-
-const SetDateCalendar = ({ onNavigate, date, setOpen }: SetDateCalendarProps) => {
-  const [view, setView] = useState<'month' | 'year' | 'decade' | 'century'>('year')
-
-  const onViewChange = ({ action, activeStartDate, view }: OnArgs) => {
-    if (action === 'drillDown' && view === 'month') {
-      onNavigate(Navigate.DATE, activeStartDate as Date)
-      setOpen(false)
-      return
-    }
-
-    setView(view)
-  }
-
-  return (
-    <SetDateCalendarContainer>
-      <CalendarComponent
-        locale="ko"
-        value={date}
-        view={view}
-        onViewChange={onViewChange}
-        next2Label={null}
-        prev2Label={null}
-      />
-    </SetDateCalendarContainer>
-  )
-}
-
-const SetDateCalendarContainer = styled.div`
-  .react-calendar {
-    border: 1px solid var(--bs-gray-300);
-    border-radius: 0.5rem;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    width: 20rem;
-  }
-
-  .react-calendar__year-view__months {
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-  }
-
-  .react-calendar__year-view .react-calendar__tile,
-  .react-calendar__decade-view .react-calendar__tile,
-  .react-calendar__century-view .react-calendar__tile {
-    padding: 1rem 0.5rem;
-
-    font-weight: 400;
-  }
-`
+import { memo, useCallback, useEffect, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import CreateEventModal from 'components/schedule/ScheduleCreateModal'
+import ScheduleCreateModal from 'components/schedule/ScheduleCreateModal'
+import { getEvent } from 'api/EventAPI'
+import { EventData } from 'interfaces/event'
+import ScheduleDetailModal from 'components/schedule/ScheduleDetailModal'
 
 interface DateCellWrapperProps {
   children: React.ReactNode
   value: Date
 }
 
-const DateCellWrapper = ({ children, value }: DateCellWrapperProps) => {
-  const handleAddEvent = () => {
-    // 일정 생성 로직
-    console.log(`일정 생성: ${value}`)
-  }
+const DateCellWrapper = memo(({ children, value }: DateCellWrapperProps) => {
+  const [open, setOpen] = useState<boolean>(false)
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>(undefined)
+
+  const handleAddEvent = useCallback(() => {
+    setSelectedDate(value)
+    setOpen(true)
+  }, [])
+
+  // 모달 닫기 핸들러
+  const onRequestClose = useCallback(() => {
+    setOpen(false)
+  }, [])
 
   return (
     <DateCellContainer>
       {children}
 
       <DateCellButton onClick={handleAddEvent}>+ 일정 생성</DateCellButton>
+
+      {open && (
+        <CreateEventModal selectedDate={selectedDate || new Date()} isOpen={open} onRequestClose={onRequestClose} />
+      )}
     </DateCellContainer>
   )
-}
+})
 
 const DateCellButton = styled.div`
   display: inline-block;
@@ -218,7 +50,7 @@ const DateCellButton = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  min-width: 10rem;
+  min-width: 80%;
   border: 1px solid var(--bs-gray-300);
   border-radius: 0.5rem;
   padding: 0.5rem 0;
@@ -251,6 +83,33 @@ const DateCellContainer = styled.div`
 
 const SchedulePage = () => {
   const localizer = momentLocalizer(moment)
+  const [open, setOpen] = useState<boolean>(false)
+  const [detailOpen, setDetailOpen] = useState<boolean>(false)
+  const [selectedItem, setSelectedItem] = useState<EventData | undefined>(undefined)
+  const [events, setEvents] = useState<EventData[]>([])
+
+  const { data } = useQuery({
+    queryKey: ['events'],
+    queryFn: () => getEvent(1),
+  })
+
+  useEffect(() => {
+    if (data) {
+      setEvents(data)
+    }
+  }, [data])
+
+  const transformedEvents = events.map((event) => ({
+    ...event,
+    start: new Date(event.start),
+    end: new Date(event.end),
+  }))
+
+  const editMode = (event: EventData) => {
+    setDetailOpen(false)
+    setSelectedItem(event)
+    setOpen(true)
+  }
 
   return (
     <Container>
@@ -263,13 +122,38 @@ const SchedulePage = () => {
           localizer={localizer}
           startAccessor="start"
           endAccessor="end"
-          events={testEvnets}
+          events={transformedEvents}
+          onSelectEvent={(event) => {
+            setSelectedItem(event)
+            setDetailOpen(true)
+          }}
           components={{
             toolbar: CustomToolbar,
             dateCellWrapper: DateCellWrapper,
           }}
+          style={{ position: 'relative' }}
         />
+        {detailOpen && (
+          <ScheduleDetailModal
+            event={selectedItem}
+            editMode={editMode}
+            isOpen={detailOpen}
+            onRequestClose={() => setDetailOpen(false)}
+          />
+        )}
       </CalendarContainer>
+
+      {/* 일정 생성 모달 */}
+      {open && (
+        <ScheduleCreateModal
+          editEvent={selectedItem}
+          selectedDate={selectedItem?.start || new Date()}
+          isOpen={open}
+          onRequestClose={() => setOpen(false)}
+        />
+      )}
+
+      {/* 일정 상세 모달 */}
     </Container>
   )
 }
@@ -304,10 +188,15 @@ const CalendarContainer = styled.div`
   .rbc-header {
     padding: 0.5rem 0;
     border-left: none;
+
+    .rbc-button-link {
+      margin-bottom: 0.5rem;
+      font-weight: bold;
+    }
   }
 
   .rbc-month-view {
-    min-height: 100rem;
+    min-height: 80rem;
     min-width: 20rem;
     border: 1px solid var(--bs-gray-200);
 
@@ -319,11 +208,62 @@ const CalendarContainer = styled.div`
       text-align: left;
       padding: 0.2rem;
     }
+
+    .rbc-event {
+      background-color: var(--bs-white);
+      color: var(--bs-gray-800);
+      padding: 0.5rem;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      box-sizing: border-box;
+
+      ::before {
+        position: absolute;
+        content: '';
+        height: 80%;
+        width: 4px;
+        left: 5px;
+        top: 50%;
+        transform: translateY(-50%);
+        border-radius: 0.5rem;
+        display: block;
+        clear: both;
+        background-color: var(--color-primary);
+      }
+    }
   }
 
   .rbc-time-view {
     .rbc-time-slot {
       padding: 5px 0;
     }
+
+    .rbc-events-container {
+      margin-right: 0;
+    }
+
+    .rbc-event {
+      border-radius: 0.5rem;
+      padding: 0.5rem;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      box-sizing: border-box;
+      background-color: var(--color-primary);
+      color: var(--bs-gray-800);
+
+      .rbc-event-label {
+        font-size: 0.8rem;
+        font-weight: bold;
+      }
+
+      .rbc-event-content {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+    }
+  }
+
+  .rbc-row-segment {
+    padding: 5px;
+    position: relative;
   }
 `
