27808b374d150e1ffcd1489cfecc31578731192f
cheiru94 <cheiru94@gmail.com>
2024-03-24 17:17:26 +0900

    fix: 스타일 지정시 오류로 변수 수정

--- src/components/icons/XIcon.tsx
+++ src/components/icons/XIcon.tsx
@@ -1,19 +1,14 @@
 import styled, { CSSProp } from 'styled-components'
 
 interface XProps {
-  style: CSSProp
+  $width?: string
+  $height?: string
+  $color?: string
 }
 
 const XIcon = ({ ...props }: XProps) => {
   return (
-    <XIconStyled
-      xmlns="http://www.w3.org/2000/XIconStyled"
-      width="1200"
-      height="1227"
-      viewBox="0 0 1200 1227"
-      fill="none"
-      {...props}
-    >
+    <XIconStyled viewBox="0 0 1200 1227" fill="none" {...props}>
       <g clip-path="url(#clip0_1_2)">
         <path
           d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z"
@@ -32,5 +27,7 @@ const XIcon = ({ ...props }: XProps) => {
 export default XIcon
 
 const XIconStyled = styled.svg<XProps>`
-  ${(props) => props.style};
+  width: ${(props) => props.$width};
+  height: ${(props) => props.$height};
+  fill: ${(props) => props.$color};
 `
