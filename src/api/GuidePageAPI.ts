c20b595d7bdeec89dd540f429e14ec09783568d9
cheiru94 <cheiru94@gmail.com>
2024-04-03 14:02:51 +0900

    feat: 가이드 페이지 API 처리

--- src/api/GuidePageAPI.ts
+++ src/api/GuidePageAPI.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { GLOBAL_CONFIG } from 'global.config'
 import { GuideInfo, Pagination } from 'interfaces/guideInfo'
+import { useState } from 'react'
 
 export const api = axios.create({
   baseURL: GLOBAL_CONFIG.VITE_APP_BASE_URL,
@@ -13,9 +14,20 @@ export const api = axios.create({
 // }
 
 export const getGuideList = async ({ cursor, limit }: { cursor?: number; limit?: number }) => {
-  const response = await api.get<Pagination<GuideInfo>>(`guides/search`)
-  return response.data.items
+  const response = await api.get<Pagination<GuideInfo>>(`guides/search`, {
+    params: {
+      cursor,
+      limit,
+    },
+  })
+  console.log('response: ', response)
+  return response.data
 }
+// export const getGuideList = async ({ cursor, limit }: { cursor?: number; limit?: number }) => {
+//   const response = await api.get<Pagination<GuideInfo>>(`guides/search`)
+//   console.log('response: ', response)
+//   return response.data
+// }
 
 export const getSelectedGuide = async (id: number) => {
   const response = await api.get(`guides/${id}`)
