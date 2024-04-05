1793b3298ad9b264466106367ac93a8d5a4e2b4a
cheiru94 <cheiru94@gmail.com>
2024-04-05 23:24:59 +0900

    feat: 필터링 API 추가

--- src/api/GuidePageAPI.ts
+++ src/api/GuidePageAPI.ts
@@ -1,35 +1,61 @@
 import axios from 'axios'
 import { GLOBAL_CONFIG } from 'global.config'
-import { GuideInfo, Pagination } from 'interfaces/guideInfo'
-import { useState } from 'react'
+import { GuideProfile, Pagination } from 'interfaces/guideInfo'
+import { RequestParamsType } from 'interfaces/requestParams'
+import { SelecteddatasAtomType } from 'state/store/SelecteddatasAtom'
 
 export const api = axios.create({
   baseURL: GLOBAL_CONFIG.VITE_APP_BASE_URL,
   withCredentials: true,
 })
 
-// export const getGuideList = async (): Promise<GuideInfo[]> => {
-//   const response = await api.get('guides/search?sort=desc')
-//   return response.data
-// }
-
+/* 1. 가이드 리스트 기본 */
 export const getGuideList = async ({ cursor, limit }: { cursor?: number; limit?: number }) => {
-  const response = await api.get<Pagination<GuideInfo>>(`guides/search`, {
+  const response = await api.get<Pagination<GuideProfile>>(`guides/search`, {
     params: {
       cursor,
       limit,
     },
   })
-  console.log('response: ', response)
   return response.data
 }
-// export const getGuideList = async ({ cursor, limit }: { cursor?: number; limit?: number }) => {
-//   const response = await api.get<Pagination<GuideInfo>>(`guides/search`)
-//   console.log('response: ', response)
-//   return response.data
-// }
 
+/* 2. 조건 선택 가이드 리스트 */
+export const getGuideListBySelected = async ({
+  cursor,
+  limit,
+  requestParams,
+}: {
+  cursor?: number
+  limit?: number
+  requestParams: RequestParamsType
+}) => {
+  const { age, temperature, guideCount, gender, areas, languages, guideCeritifications, rating } = requestParams
+  const response = await api.get<Pagination<GuideProfile>>(`guides/search/`, {
+    params: {
+      cursor,
+      limit,
+      age,
+      temperature,
+      guideCount,
+      gender,
+      areas,
+      languages,
+      guideCeritifications,
+      rating,
+    },
+  })
+  return response.data
+}
+
+/*  3. 가이드 상세 */
 export const getSelectedGuide = async (id: number) => {
   const response = await api.get(`guides/${id}`)
+  return response.data
+}
+
+/* 4. 지역 정보 수집 */
+export const getAreaDatas = async () => {
+  const response = await api.get('areas')
   return response.data
 }
