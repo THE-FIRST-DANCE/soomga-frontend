2c18edf22b930f90e2c2111fd2f3e0e61a944c78
cheiru94 <cheiru94@gmail.com>
2024-04-07 20:42:55 +0900

    feat: 필터링 리퀘스트 타입

--- src/interfaces/requestParams.ts
+++ src/interfaces/requestParams.ts
@@ -1,10 +1,10 @@
 export interface RequestParamsType {
-  age: string
-  temperature: string
-  guideCount: string
-  gender: string | undefined
-  areas: string
-  languages: string
-  guideCeritifications: string
-  rating: string
+  age?: string
+  temperature?: string
+  guideCount?: string
+  gender?: string
+  areas?: string
+  languages?: string
+  guideCeritifications?: string
+  rating?: string
 }
