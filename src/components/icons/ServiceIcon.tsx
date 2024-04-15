e457d0ea8828445655e8a63680001e2de5871c30
cheiru94 <cheiru94@gmail.com>
2024-04-16 00:03:46 +0900

    feat: 채팅페이지 서비스 아이콘

---
+++ src/components/icons/ServiceIcon.tsx
@@ -0,0 +1,38 @@
+import styled, { CSSProp } from 'styled-components'
+
+interface ServiceIconProps {
+  style: CSSProp
+  $hoverColor?: string
+  onClick: () => void
+}
+
+const ServiceIcon = ({ ...props }: ServiceIconProps) => {
+  return (
+    <PlanIconStyle
+      className="feather feather-shopping-bag"
+      fill="none"
+      height="24"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="1"
+      viewBox="0 0 24 24"
+      width="24"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z" />
+      <line x1="3" x2="21" y1="6" y2="6" />
+      <path d="M16 10a4 4 0 0 1-8 0" />
+    </PlanIconStyle>
+  )
+}
+
+export default ServiceIcon
+
+const PlanIconStyle = styled.svg<ServiceIconProps>`
+  ${(props) => props.style}
+  &:hover {
+    stroke: ${(props) => props.$hoverColor || 'gray'};
+  }
+`
