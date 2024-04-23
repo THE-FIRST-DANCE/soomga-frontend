94396a7551aea08749593e90e4d0c9d1ea7a63fa
Juhyeon Lee <l.juhyeonni@gmail.com>
2024-04-24 03:28:07 +0900

    Update global.config.ts

--- src/global.config.ts
+++ src/global.config.ts
@@ -1,3 +1,3 @@
 export const GLOBAL_CONFIG = {
-  VITE_APP_BASE_URL: import.meta.env.VITE_APP_BASE_URL || 'http://localhost:3000/api/',
+  VITE_APP_BASE_URL: import.meta.env.VITE_APP_BASE_URL || '',
 }
